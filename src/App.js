66deb9773caec924579fa689e9adc07002709051
Adam Workman <adammworkman25@gmail.com>
2021-01-06 14:42:17 -0500

    display intro

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.scss';
-import Intro from './components/intro'
+import Intro from './components/Intro'
+import DisplayIntro from './components/DisplayIntro'
 
 function App() {
   return (
 <div>
   <Intro/>
+  <div className="intro" id="intro"> 
+  <DisplayIntro/>
+  </div>
 </div>
   );
 }
