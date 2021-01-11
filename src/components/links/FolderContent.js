24edfb37659e62b300d10d062bd7fca28d6f29d0
Adam Workman <adammworkman25@gmail.com>
2021-01-10 22:22:50 -0500

    add folder content to links

---
+++ src/components/links/FolderContent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Linkedin from './Linkedin'
+import Github from './GitHub'
+import Medium from './Medium'
+
+const FolderContent = () => {
+  return (
+    <div>
+      <Linkedin/>
+      <Github/>
+      <Medium/>      
+    </div>
+  );
+};
+
+export default FolderContent;
