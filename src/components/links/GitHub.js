749aa9e52758178dc0b5e6b90f3e8d2f5bc338fd
Adam Workman <adammworkman25@gmail.com>
2021-01-10 22:23:03 -0500

    show githuib logo

---
+++ src/components/links/GitHub.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GitHub extends Component {
+  render() {
+    return (
+      <div className='linkLogo linkLogo--github'>
+        <a href='https://github.com/Amworkman/' target="_blank" rel="noreferrer"><img className='linkImg' src='/github-logo.png' width='100%' height='auto'/></a>
+      </div>
+    );
+  }
+}
+
+export default GitHub;
