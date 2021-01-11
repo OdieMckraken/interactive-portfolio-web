4fdb4f960394f497a9b2ab4370d9611979da29af
Adam Workman <adammworkman25@gmail.com>
2021-01-10 22:23:47 -0500

    covid card

--- src/components/projects/CovidTrackerCard.js
+++ src/components/projects/CovidTrackerCard.js
@@ -5,9 +5,33 @@ class CovidTrackerCard extends Component {
     return (
       <div className='projectCard projectCard--covid'>
         <div className='projectCardInner'>
-          <div className='projectCardFace projectCardFace--front' style={{ backgroundImage: "url(/covid-tracker.png)" }}>
+          <div className='projectCardFace projectCardFace--front-covid' style={{ backgroundImage: "url(/covid-tracker.png)" }}>
           </div>
-          <div className='projectCardFace projectCardFace--back'>
+          <div className='projectCardFace projectCardFace--back-covid'>            
+              <h1> Covid-19 Tracker</h1><br/>
+            <div className='textBox'>
+              <p> Covid-19 has become a huge part of our lives.
+                It has influence over nearly all of our decisions.
+                it has changed the way our society works, and it has 
+                made us more aware than ever of how fragile our lives really are. 
+              </p>
+              <p>
+                With Covid-19 Tracker, you can check on the current
+                status of the pandemic in any country. You can view the country
+                with the highest number of confirmed cases, or the highest number of new cases.
+                You can see a list of all the countries that have no new cases.
+                You can get the stats of a random country, or search for the country 
+                You are most interested in. There is even a hidden surprise!
+              </p>
+              <p>
+                This was the fist program that I built with ruby.
+                It is simple and functional.
+              </p>
+              <p>
+                You can check it out here 
+              </p>          
+            </div>
+            <a href="https://github.com/Amworkman/covid_tracker" target="_blank" rel="noreferrer"><img border="0" alt="W3Schools" src="/GitHub-Mark-Light-64px.png" width="50vw" height="50vw"/></a>
           </div>
         </div>        
       </div>
