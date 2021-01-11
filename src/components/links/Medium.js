7ac82b021708c5a3cf6cbeb49d002ac922763214
Adam Workman <adammworkman25@gmail.com>
2021-01-10 22:23:27 -0500

    show medium logo

---
+++ src/components/links/Medium.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Medium extends Component {
+  render() {
+    return (
+      <div className='linkLogo linkLogo--medium'>
+        <a href='https://adammworkman25.medium.com/' target="_blank" rel="noreferrer"><img className='linkImg' src='/medium-logo.png' width='100%' height='auto'/></a>
+      </div>
+    );
+  }
+}
+
+export default Medium;
