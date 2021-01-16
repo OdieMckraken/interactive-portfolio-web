4ce2d23317c26f73607c22cb5ad4ad284309e44a
Adam Workman <adammworkman25@gmail.com>
2021-01-16 18:11:52 -0500

    force https

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux'
 import {createStore} from 'redux'
 import FoldersReducer from './reducers/FoldersReducer'
+import HttpsRedirect from 'react-https-redirect';
 
 const store = createStore(FoldersReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(  
-  <Provider store={store}>
-    <App /> 
-  </Provider>,
+  <HttpsRedirect>
+    <Provider store={store}>
+      <App /> 
+    </Provider>,
+  </HttpsRedirect>,
   document.getElementById('root')
 );
 
