d1eafeb0a9acf0be0611996ab0db7dfa51ae7239
Adam Workman <adammworkman25@gmail.com>
2021-03-10 09:47:26 -0500

    Fix typo and change link

--- src/components/projects/KatchupsCard.js
+++ src/components/projects/KatchupsCard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class KatchupsCard extends Component {
   handleClick = () => {
-    window.open('https://amworkman.github.io/katchups-front/')
+    window.open('https://youtu.be/_KOrnDga8-k')
   }  
   render() {
     return (
@@ -17,7 +17,7 @@ class KatchupsCard extends Component {
                 there was an easier way to decide on a restaurant?
               </p>
               <p>
-                katchups gives you and easy and convenient way to choose 
+                katchups gives you an easy and convenient way to choose 
                 a restaurant that you will both enjoy! When you sign into katchups,
                 you will see a list of your friends on the right. Just pick a friend and 
                 get ready for the magic. After you pick a date, time and location, you
@@ -26,12 +26,11 @@ class KatchupsCard extends Component {
                 until a match is found... voila!   
               </p>
               <p>
-                katchups was built using a rails API and a vanilla javascipt 
-                frontend. It is currently undergoing a large refactor and not all of the
-                features will be functional.
+                katchups was built using a rails API and a ReactJS
+                frontend. This is my current project and is being updated daily.
               </p>
               <p>
-                Click to check it out! 
+                Click to see a demo! 
               </p>
             </div>
           </div>
